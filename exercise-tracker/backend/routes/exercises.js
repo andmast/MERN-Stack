b4565d5406831de57b0a286818ad7b30232571b8
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-11-19 13:14:54 -0500

    Added exercise delete route with id param

--- exercise-tracker/backend/routes/exercises.js
+++ exercise-tracker/backend/routes/exercises.js
@@ -27,4 +27,10 @@ router.route("/:id").get((req, res) => {
     .catch(err => res.status(400).json("Error:" + err));
 });
 
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise Deleted"))
+    .catch(err => res.status(400).json("Error:" + err));
+});
+
 module.exports = router;
